003035ca3aaacb3d11848698ff0317af89b5c1a9
Lee-Da-Hye <dlekgp6521@naver.com>
2024-02-25 23:32:17 +0900

    modify dust

--- js/weather.js
+++ js/weather.js
@@ -1,67 +1,97 @@
 $(document).ready(function() {
-    var serviceKey = 'LukiVznMrRVfgr%2FmME%2Flropqo%2Fn39Hq74bNK9gZh1%2B8oW%2BLAi70wMvDqzgXK3cAyZzlWSKjieh68kBnwM8TQoQ%3D%3D';
-    var airUrl = `https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty`;
-    const sidoName = "서울";
+  var serviceKey = 'LukiVznMrRVfgr%2FmME%2Flropqo%2Fn39Hq74bNK9gZh1%2B8oW%2BLAi70wMvDqzgXK3cAyZzlWSKjieh68kBnwM8TQoQ%3D%3D';
+  var airUrl = `https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty`;
+  const sidoName = "서울";
 
-    let params = {
-      serviceKey: serviceKey,
-      numOfRows: '1',
-      pageNo: '1',
-      dataTerm: 'MONTH',
-      ver: '1.3',
-      stationName: '마포구',
-      sidoName: sidoName,
-      returnType: 'json'
-  };
+let params = {
+    serviceKey: serviceKey,
+    numOfRows: '1',
+    pageNo: '1',
+    dataTerm: 'MONTH',
+    ver: '1.3',
+    stationName: '마포구',
+    sidoName: sidoName,
+    returnType: 'json'
+};
 
-  $.ajax({
-      url: airUrl,
-      method: 'GET',
-      data: params,
-      success: function(response) {
-          const items = response.response.body.items;
-          // 미세먼지 및 오존 농도 표시
-          const pm10 = getDustStatus(items);
-          const o3 = getOzoneStatus(items);
-          updateDustInfo(sidoName,pm10, o3);
-          // 미세먼지 및 오존 이미지 업데이트
-          updateStatusImage(pm10, o3);
-      },
-      error: function(error) {
-          console.log(error);
-      }
-  });
+$.ajax({
+    url: airUrl,
+    method: 'GET',
+    data: params,
+    success: function(response) {
+        const items = response.response.body.items;
+        // 미세먼지 및 오존 농도 표시
+        const pm10 = getDustStatus(items);
+        const o3 = getOzoneStatus(items);
+        const pm10Status = getStatus(pm10);
+        const o3Status = getOzoneStatusText(o3);
+        updateDustInfo(sidoName, pm10Status, o3Status);
+        // 미세먼지 및 오존 이미지 업데이트
+        updateStatusImage(pm10Status, o3Status);
+    },
+    error: function(error) {
+        console.log(error);
+    }
 });
 
 function getDustStatus(items) {
-  return items[0].pm10Value; // 단위: 마이크로그램/미터³
+    return items[0].pm10Value; // 단위: 마이크로그램/미터³
 }
 
 function getOzoneStatus(items) {
-  return items[0].o3Value; // 단위: 마이크로그램/미터³
+    return items[0].o3Value; // 단위: 마이크로그램/미터³
+}
+
+function getStatus(value) {
+    if (value <= 30) {
+        return "좋음";
+    } else if (value <= 80) {
+        return "보통";
+    } else {
+        return "나쁨";
+    }
+}
+
+function getOzoneStatusText(value) {
+    if (value <= 0.03) {
+        return "좋음";
+    } else if (value <= 0.09) {
+        return "보통";
+    } else {
+        return "나쁨";
+    }
 }
 
 function updateDustInfo(location, pm10, o3) {
-  $('#location').text('지역: ' + location);
-  $('#dustStatusText').text('미세먼지 농도: ' + pm10 + ' μg/m³');
-  $('#ozoneStatusText').text('오존 농도: ' + o3 + ' μg/m³');
+    $('#location').text('지역: ' + location);
+    $('#dustStatusText').text('미세먼지 상태: ' + pm10);
+    $('#ozoneStatusText').text('오존 상태: ' + o3);
 }
 
 function updateStatusImage(pm10, o3) {
-  var imageUrl1 = getImageUrl(pm10);
-  var imageUrl2 = getImageUrl(o3);
-  $('#statusImage1').attr('src', imageUrl1);
-  $('#statusImage2').attr('src', imageUrl2);
+    var imageUrl1 = getStatusImageUrl(pm10);
+    var imageUrl2 = getOzoneStatusImageUrl(o3);
+    $('#statusImage1').attr('src', imageUrl1);
+    $('#statusImage2').attr('src', imageUrl2);
 }
 
-function getImageUrl(value) {
-  if (value <= 30) {
-      return "img/weather/good.png"; // 좋음 상태 이미지
-  } else if (value <= 80) {
-      return "img/weather/normal.png"; // 보통 상태 이미지
-  } else {
-      return "img/weather/bad.png"; // 나쁨 상태 이미지
-  }
+function getStatusImageUrl(status) {
+    if (status === "좋음") {
+        return "img/weather/good.png"; // 좋음 상태 이미지
+    } else if (status === "보통") {
+        return "img/weather/normal.png"; // 보통 상태 이미지
+    } else {
+        return "img/weather/bad.png"; // 나쁨 상태 이미지
+    }
 }
 
-
+function getOzoneStatusImageUrl(status) {
+    if (status === "좋음") {
+        return "img/weather/good.png"; // 오존 좋음 상태 이미지
+    } else if (status === "보통") {
+        return "img/weather/normal.png"; // 오존 보통 상태 이미지
+    } else {
+        return "img/weather/bad.png"; // 오존 나쁨 상태 이미지
+    }
+}
+})
